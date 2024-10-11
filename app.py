aae909b8d9caf459ef4d58d23a394309ef8e0529
toxicNinja04 <gupta.manish2304@gmail.com>
2024-10-11 20:49:19 +0530

    added sample data and functionality

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-from flask import Flask, url_for, render_template, jsonify
+from flask import Flask, url_for, render_template, jsonify,request
 
 app = Flask(__name__)
+
+laptops = [
+    {"name": "Laptop A", "price": 50000, "purpose": "work"},
+    {"name": "Laptop B", "price": 60000, "purpose": "gaming"}
+]
 
 @app.route('/')
 
 def home():
     return render_template('index.html')
 
+
 @app.route('/recommend', methods=['POST'])
 def recommend():
-    
-    return jsonify({"message": "Recommendation system under development"})
+    budget = int(request.form.get('budget'))
+    purpose = request.form.get('purpose')
+    recommended_laptops = [laptop for laptop in laptops if laptop['price'] <= budget and laptop['purpose'] == purpose]
+    return jsonify({"message": f"Recommended Laptops: {recommended_laptops}"})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
